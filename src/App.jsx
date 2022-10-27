f5de0f52901b8fed955bcbf95689a23ebed8f76c
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-10-27 02:42:13 -0300

    Structure & Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
+import {Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, 
+  Clients, CTA, Footer } from './components'
 import styles from "./style"
+import './input.css'
+
+
 
 /*https://www.youtube.com/watch?v=_oO4Qi5aVZs*/ 
 
 const App = () => {
   return (
-    <div className="bg-primary w-full overflow-hidden">
+    <div className="bg-black w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-            Navbar
+           <Navbar />
         </div>
+
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Hero />
+          </div>
+        </div>
+
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Stats />
+            <Business />
+            <Billing />
+            <CardDeal />
+            <Testimonials />
+            <Clients />
+            <CTA />
+            <Footer />
+          </div>
+        </div>
+
       </div>
     </div>
   )
---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Hero from "./Hero";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+ 
+export {
+    Navbar, 
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero
+};
